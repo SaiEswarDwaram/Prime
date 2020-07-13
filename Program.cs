2b03ce5888177dfeb373883f3956b302e90a4cbe
SaiEswarDwaram <1210316012@gitam.in>
2020-07-13 16:13:01 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,num;
+            int value = 0;
+
+            Console.WriteLine("Enter the number to be checked");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (i=1;i<=num;i++)
+            {
+                if (num % i == 0)
+                    value = value + 1;
+            }
+            if (value == 2)
+                Console.WriteLine("The number is prime");
+            else
+                Console.WriteLine("The number is not prime");
+        }
+    }
+}
